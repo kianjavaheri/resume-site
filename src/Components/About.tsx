8a2a6570938f9961e4f4915184938812e93b604a
Kian Javaheri <kianjavaheri911@gmail.com>
2022-04-02 00:08:34 -0700

    Finishing Up 4/2/22

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -6,8 +6,8 @@ function About() {
         <h1 className="about-header">About Me</h1>
         <section className="about-container">
             <p>
-                {/* My name is Kian Javaheri. I'm an aspiring back-end software engineer from the Bay Area in California. Currently, I am studying computer science with a minor in cognitive science at the University of Minnesota-Twin Cities. */}
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus beatae iure, repudiandae, odit praesentium accusantium magni commodi est sint distinctio eveniet sunt blanditiis aliquam asperiores quae in necessitatibus ex! Praesentium.
+                My name is Kian Javaheri. I'm 17 years old and I'm an aspiring back-end software engineer from the Bay Area in California. Currently, I am high school student but I plan to study computer science and philosophy in university!
+                {/* Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus beatae iure, repudiandae, odit praesentium accusantium magni commodi est sint distinctio eveniet sunt blanditiis aliquam asperiores quae in necessitatibus ex! Praesentium. */}
             </p>
         </section>
 
