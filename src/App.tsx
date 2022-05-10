9aea3900a1b21f6c9225080c0a2d235e6b5333d1
Kian Javaheri <kianjavaheri911@gmail.com>
2022-05-10 11:30:19 -0700

    5/10/22 - 11:30 AM
    scratched the new proficiency

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import Navbar from './Components/Home/Navbar';
 import Proficiency from './Components/Home/Proficiency';
 import ScrollButton from './Components/Home/Scroll';
 import Projects from './Components/Home/Projects';
-import ProficiencyTest from './Components/Home/ProficiencyTest';
 
 
 const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -30,13 +29,12 @@ function HomePage() {
     <div className="App" data-theme={theme}>
         <Navbar switchTheme={switchTheme} isChecked={isChecked}/>
         <About/>
-        {/* <hr className="divider"/> */}
-        {/* <Projects/> */}
         <hr className="divider"/>
-        <ProficiencyTest />
-        {/* <Proficiency/> */}
-        {/* <hr className="divider"/>
-        <Contact /> */}
+        <Projects/>
+        <hr className="divider"/>
+        <Proficiency/>
+        <hr className="divider"/>
+        <Contact />
         <ScrollButton/>
     </div>
   );
