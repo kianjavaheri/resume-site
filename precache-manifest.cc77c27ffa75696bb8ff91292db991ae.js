d703556e1efdeae52cd49c27021e218c9c63d899
Kian Javaheri <kianjavaheri911@gmail.com>
2022-09-07 03:59:32 -0700

    Updates

---
+++ precache-manifest.cc77c27ffa75696bb8ff91292db991ae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "35c109230ed9ab8587ca",
+    "url": "/resume-site/static/css/main.42d14410.chunk.css"
+  },
+  {
+    "revision": "35c109230ed9ab8587ca",
+    "url": "/resume-site/static/js/main.35c10923.chunk.js"
+  },
+  {
+    "revision": "9b8eaa66f4b4aebdb83c",
+    "url": "/resume-site/static/js/1.9b8eaa66.chunk.js"
+  },
+  {
+    "revision": "4ba565165f614f332102",
+    "url": "/resume-site/static/js/runtime~main.4ba56516.js"
+  },
+  {
+    "revision": "a7e91e0734c604421d9f556f05a1a9ac",
+    "url": "/resume-site/static/media/pythonlogo.a7e91e07.svg"
+  },
+  {
+    "revision": "f8857fd220e93a7b70f80f1c34f1b332",
+    "url": "/resume-site/static/media/javalogo.f8857fd2.svg"
+  },
+  {
+    "revision": "ef58f40bbd22fa7cf47ed6aa5ca5deee",
+    "url": "/resume-site/static/media/javascriptlogo.ef58f40b.svg"
+  },
+  {
+    "revision": "7db11b8dfacca0e8aba3f795930d9db4",
+    "url": "/resume-site/static/media/reactlogo.7db11b8d.svg"
+  },
+  {
+    "revision": "85ef088a47df17df2f8485a78d389683",
+    "url": "/resume-site/static/media/uparrow.85ef088a.svg"
+  },
+  {
+    "revision": "1f4902d582c61cbe15ceddd922b5102f",
+    "url": "/resume-site/index.html"
+  }
+];
