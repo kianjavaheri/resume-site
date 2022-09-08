e20c94960e3fb42764b614f31beff201d68a1ccb
Kian Javaheri <kianjavaheri911@gmail.com>
2022-09-08 08:33:11 -0700

    Updates

--- precache-manifest.169c6dbc933d4de6f71d401d45d6c36e.js
+++ precache-manifest.169c6dbc933d4de6f71d401d45d6c36e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6211b70cbb8d286d7444",
-    "url": "/resume-site/static/css/main.336a7eec.chunk.css"
+    "revision": "826b0f33e48c6c957da6",
+    "url": "/resume-site/static/css/main.6da61c25.chunk.css"
   },
   {
-    "revision": "6211b70cbb8d286d7444",
-    "url": "/resume-site/static/js/main.28a385a0.chunk.js"
+    "revision": "826b0f33e48c6c957da6",
+    "url": "/resume-site/static/js/main.52779480.chunk.js"
   },
   {
     "revision": "575e873410de6d70e939",
@@ -36,7 +36,7 @@ self.__precacheManifest = [
     "url": "/resume-site/static/media/uparrow.85ef088a.svg"
   },
   {
-    "revision": "c0e0f60ee9a347e2f719596e9f0c32fc",
+    "revision": "2c201bf064fddd35cdd2f94408a10d15",
     "url": "/resume-site/index.html"
   }
 ];
