5fe58934168d7753aecadd54123933e700a7231e
Kian Javaheri <kianjavaheri911@gmail.com>
2022-09-08 08:38:57 -0700

    Updates

---
+++ precache-manifest.c31c6ea91c011617f1caccd74310c95f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "826b0f33e48c6c957da6",
+    "url": "/static/css/main.6da61c25.chunk.css"
+  },
+  {
+    "revision": "826b0f33e48c6c957da6",
+    "url": "/static/js/main.52779480.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a8a59f998d3eb0ad94b",
+    "url": "/static/js/2.c549c271.chunk.js"
+  },
+  {
+    "revision": "a7e91e0734c604421d9f556f05a1a9ac",
+    "url": "/static/media/pythonlogo.a7e91e07.svg"
+  },
+  {
+    "revision": "f8857fd220e93a7b70f80f1c34f1b332",
+    "url": "/static/media/javalogo.f8857fd2.svg"
+  },
+  {
+    "revision": "ef58f40bbd22fa7cf47ed6aa5ca5deee",
+    "url": "/static/media/javascriptlogo.ef58f40b.svg"
+  },
+  {
+    "revision": "7db11b8dfacca0e8aba3f795930d9db4",
+    "url": "/static/media/reactlogo.7db11b8d.svg"
+  },
+  {
+    "revision": "85ef088a47df17df2f8485a78d389683",
+    "url": "/static/media/uparrow.85ef088a.svg"
+  },
+  {
+    "revision": "a2f4e7b287fc64085aadeae4be113caf",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/resume-site/precache-manifest.169c6dbc933d4de6f71d401d45d6c36e.js"
+  "/precache-manifest.c31c6ea91c011617f1caccd74310c95f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/resume-site/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
