648a1c34fe4236bf78d0af9525ece658cea6e2a2
Kian Javaheri <kianjavaheri911@gmail.com>
2022-09-08 00:00:45 -0700

    asd
    asd

--- README.md
+++
@@ -1,4 +0,0 @@
-This is my resume site!
-
-
-Created by Kian Javaheri - kianjavaheri911@gmail.com
---
+++ build/precache-manifest.fa78fda418033d0eb339a6c4e4b233a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6211b70cbb8d286d7444",
+    "url": "/resume-site/static/css/main.336a7eec.chunk.css"
+  },
+  {
+    "revision": "6211b70cbb8d286d7444",
+    "url": "/resume-site/static/js/main.28a385a0.chunk.js"
+  },
+  {
+    "revision": "575e873410de6d70e939",
+    "url": "/resume-site/static/js/runtime~main.9454f64d.js"
+  },
+  {
+    "revision": "2a8a59f998d3eb0ad94b",
+    "url": "/resume-site/static/js/2.c549c271.chunk.js"
+  },
+  {
+    "revision": "a7e91e0734c604421d9f556f05a1a9ac",
+    "url": "/resume-site/static/media/pythonlogo.a7e91e07.svg"
+  },
+  {
+    "revision": "f8857fd220e93a7b70f80f1c34f1b332",
+    "url": "/resume-site/static/media/javalogo.f8857fd2.svg"
+  },
+  {
+    "revision": "ef58f40bbd22fa7cf47ed6aa5ca5deee",
+    "url": "/resume-site/static/media/javascriptlogo.ef58f40b.svg"
+  },
+  {
+    "revision": "7db11b8dfacca0e8aba3f795930d9db4",
+    "url": "/resume-site/static/media/reactlogo.7db11b8d.svg"
+  },
+  {
+    "revision": "85ef088a47df17df2f8485a78d389683",
+    "url": "/resume-site/static/media/uparrow.85ef088a.svg"
+  },
+  {
+    "revision": "c0e0f60ee9a347e2f719596e9f0c32fc",
+    "url": "/resume-site/index.html"
+  }
+];
