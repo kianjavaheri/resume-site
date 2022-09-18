4cbb0aee8607be7ffb0bd6b24961a5cd42fb3cbf
Kian Javaheri <kianjavaheri911@gmail.com>
2022-09-18 01:24:47 -0700

    9/18/22 - 1:24 AM
    idk

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    {/* <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={ <App/>}/>
       </Routes>
-    </HashRouter>
+    </HashRouter> */}
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={ <App/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
