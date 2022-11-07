40df2025f5c74a13243a52b6bef80cdce81e42d4
Kian Javaheri <kianjavaheri911@gmail.com>
2022-11-06 18:01:04 -0700

    11/6/22 - 6:01 PM
    econ too

--- src/Components/Home/About.tsx
+++ src/Components/Home/About.tsx
@@ -7,7 +7,7 @@ function About() {
         <h1 className="about-header">About Me</h1>
         <section className="about-container">
             <p>
-                My name is Kian Javaheri. I was born and raised in the Bay Area in California. Currently, I am an 18 year-old computer science student at Barrett, The Honors College at Arizona State University. I am looking forward to working in full-stack development and potentially machine learning/artificial intelligence!
+                My name is Kian Javaheri. I was born and raised in the Bay Area in California. Currently, I am an 18 year-old computer science and economics student at Barrett, The Honors College at Arizona State University. I am looking forward to working in full-stack development and potentially machine learning/artificial intelligence!
             </p>
         </section>
 
