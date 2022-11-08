459af22d5607372056abe44432f8ada324586d49
Kian Javaheri <kianjavaheri911@gmail.com>
2022-11-08 05:41:41 -0700

    11/8/22 - 5:41 AM
    updated resume

--- src/Components/Home/Resume.tsx
+++ src/Components/Home/Resume.tsx
@@ -4,7 +4,7 @@ function Resume() {
   return (
     <div>
         <section className="resume-container">
-            <a href="https://drive.google.com/file/d/1B2sgk7Omvsf22ewzZkY5IYNlg0c8g9gz/view?usp=sharing" className="resume-button" target="_blank" rel="noopener noreferrer">Resume</a>
+            <a href="https://drive.google.com/file/d/1n7ibELGilE0ELvksT3jF66Lxq4S0uYPM/view?usp=sharing" className="resume-button" target="_blank" rel="noopener noreferrer">Resume</a>
         </section>
     </div>
   )
